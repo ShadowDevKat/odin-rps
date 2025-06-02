a24b7e68d9cf7f3a4d95bcd8af17c77b450ced67
ShadowDevKat <211905275+ShadowDevKat@users.noreply.github.com>
2025-06-02 12:43:22 +0500

    Added method to play a single round

--- rps.js
+++ rps.js
@@ -16,8 +16,31 @@ function getHumanChoice() {
     return humanChoice.toLowerCase();
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+// console.log(getComputerChoice());
+// console.log(getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice === computerChoice) {
+        alert(`Tie!! You both chose ${humanChoice}`);
+    }
+    else if(humanChoice === "rock" && computerChoice === "scissors") {
+        alert("You Win! rock beats scissors");
+    }
+    else if(humanChoice === "paper" && computerChoice === "rock") {
+        alert("You Win! paper beats rock");
+    }
+    else if(humanChoice === "scissors" && computerChoice === "paper") {
+        alert("You Win! scissors beats paper");
+    }
+    else {
+        alert(`You Lose! ${computerChoice} beats ${humanChoice}`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection);
