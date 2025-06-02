bb7501a8661bccd90a69496abbbcd605d6a6bd09
ShadowDevKat <211905275+ShadowDevKat@users.noreply.github.com>
2025-06-02 12:12:35 +0500

    Added method to get computer choice

--- rps.js
+++ rps.js
@@ -1,1 +1,32 @@
-console.log("Hello!");
+// console.log("Hello!");
+
+let rock = "rock";
+let paper = "paper";
+let scissor = "scissor";
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() {
+    let randomNumber = getRandomInt(3);
+    let randomChoice = "";
+
+    switch(randomNumber) {
+        case 0:
+            randomChoice = rock;
+            break;
+        case 1:
+            randomChoice = paper;
+            break;
+        case 2:
+            randomChoice = scissor;
+            break;
+        default:
+            break;
+    }
+
+    return randomChoice;
+}
+
+console.log(getComputerChoice());
